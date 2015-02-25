5fa4618a5d744414515f6aae3612d4156858a04c
KleHe <klemens.henk@gmx.at>
2015-02-25 23:23:39 +0100

    Timemodel: Variablen angelegt

--- rpntts-webgui/modules/models/TimeModel.php
+++ rpntts-webgui/modules/models/TimeModel.php
@@ -4,4 +4,14 @@ namespace modules\models;
 
 class TimeModel {
 
+    private $modelName = '';
+    private $primaryKey = 0;
+    private $monday = 0;
+    private $tuesday = 0;
+    private $wednesday = 0;
+    private $thursday = 0;
+    private $friday = 0;
+    private $saturday = 0;
+    private $sunday = 0;
+    
 }
