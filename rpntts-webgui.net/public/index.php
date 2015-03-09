82f7e6dfbf7b6cdc946be68d6c6f402450a6bcf1
Klemens Henk <klemens.henk@wogibtswas.at>
2015-03-09 19:17:03 +0100

    Zend Autoloader

--- rpntts-webgui.net/public/index.php
+++ rpntts-webgui.net/public/index.php
@@ -1,3 +1,11 @@
 <?php
-phpinfo();
+require_once(
+__DIR__ .
+'/../vendor/zendframework/library/Zend/Loader/AutoloaderFactory.php'
+);
+Zend/Loader/AutoloaderFactory::factory(array(
+    'Zend/Loader/StandardAutoloader' => array(
+    'autoregister_zf' => true
+    )
+));
 ?>
