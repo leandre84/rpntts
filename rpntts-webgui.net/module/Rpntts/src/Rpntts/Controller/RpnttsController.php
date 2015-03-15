f523bba9c09bc27cc9de1f61d441432191557f18
KleHe <klemens.henk@gmx.at>
2015-03-15 21:14:24 +0100

    Webgui: Komplettumbau

---
+++ rpntts-webgui.net/module/Rpntts/src/Rpntts/Controller/RpnttsController.php
@@ -0,0 +1,23 @@
+namespace Rpntts\Controller;
+
+ use Zend\Mvc\Controller\AbstractActionController;
+ use Zend\View\Model\ViewModel;
+
+ class RpnttsController extends AbstractActionController
+ {
+     public function indexAction()
+     {
+     }
+
+     public function addAction()
+     {
+     }
+
+     public function editAction()
+     {
+     }
+
+     public function deleteAction()
+     {
+     }
+ }
