ea34bae816feef4e80eb324250d67aff1c3a067b
Leandros Athanasiadis <leandre84@gmail.com>
2015-03-18 08:55:27 +0100

    Move bin_to_hex to function

---
+++ rpntts-backend/rpntts-common.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "rpntts-common.h"
+
+void bin_to_hex(uint8_t *in, uint8_t in_length, char *out) {
+    char *ppos;
+    uint16_t i;
+
+    ppos = out;
+    for(i = 0; i < in_length; i++) {
+        sprintf(ppos, "%02X", in[i]);
+        ppos += 2;
+    }
+}
