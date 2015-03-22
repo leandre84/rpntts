d379c46824ad2f3c609c4ad1e8ec6aa26defd225
KleHe <klemens.henk@gmx.at>
2015-03-22 20:27:51 +0100

    Webgui: ?

--- rpntts-webgui.net/module/Rpntts/config/module.config.php
+++ rpntts-webgui.net/module/Rpntts/config/module.config.php
@@ -1,34 +1,34 @@
 <?php
 return array(
-     'controllers' => array(
-         'invokables' => array(
-             'Rpntts\Controller\Rpntts' => 'Rpntts\Controller\RpnttsController',
-         ),
-     ),
-     
-     'router' => array(
-         'routes' => array(
-             'rpntts' => array(
-                 'type'    => 'segment',
-                 'options' => array(
-                     'route'    => '/rpntts[/:action][/:id]',
-                     'constraints' => array(
-                         'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                         'id'     => '[0-9]+',
-                     ),
-                     'defaults' => array(
-                         'controller' => 'Rpntts\Controller\Rpntts',
-                         'action'     => 'index',
-                     ),
-                 ),
-             ),
-         ),
-     ),
-
-     'view_manager' => array(
-         'template_path_stack' => array(
-             'rpntts' => __DIR__ . '/../view',
-         ),
-     ),
- );
+    'controllers' => array(
+        'invokables' => array(
+            'Rpntts\Controller\Rpntts' => 'Rpntts\Controller\RpnttsController',
+        ),
+    ),
+    
+    'router' => array(
+        'routes' => array(
+            'rpntts' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/rpntts[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Rpntts\Controller\Rpntts',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+    
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'rpntts' => __DIR__ . '/../view/rpntts/rpntts',
+        ),
+    ),
+);
  
